afcd96427f72b10a1221c7b7ba95c4d02e011460
Daniel Garcia <grciazd@gmail.com>
2021-10-28 19:54:11 -0700

    adding question 6

---
+++ conditionals/weather_part2.js
@@ -0,0 +1,13 @@
+let weather = '';
+
+switch (weather) {
+  case 'Sunny':
+    console.log("It's a beautiful day!");
+    break;
+  case 'Rainy':
+    console.log('Grab an umbrella');
+    break;
+  default:
+    console.log("Let's stay inside");
+    break;
+};
